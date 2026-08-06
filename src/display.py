4092d9fa90913515a2a1b92ef6a5c60711237d21
yohann JOLLY <yohann.j37@gmail.com>
2026-08-06 20:21:24 +0200

    added page-to-page navigation

--- src/display.py
+++ src/display.py
@@ -15,8 +15,12 @@ class Display:
 
         self.__load_pictures()
 
+        self.__load_buttons()
+
     def __load_pictures(self):
-        self.__background = pygame.image.load(os.path.join(self.__path, "assets", "pictures", "background.jpg"))
+        self.__home_background = pygame.image.load(os.path.join(self.__path, "assets", "pictures", "home_background.jpg"))
+        self.__level_background = pygame.image.load(os.path.join(self.__path, "assets", "pictures", "TEMP_level_background.jpg"))
+        self.__setting_background = pygame.image.load(os.path.join(self.__path, "assets", "pictures", "TEMP_setting_background.jpg"))
 
         self.__back_button = pygame.image.load(os.path.join(self.__path, "assets", "pictures", "back_button.jpg"))
         self.__back_button_rect = self.__back_button.get_rect(topleft=(50, 50))
@@ -25,20 +29,30 @@ class Display:
         self.__setting_button = pygame.image.load(os.path.join(self.__path, "assets", "pictures", "setting_button.jpg"))
         self.__setting_button_rect = self.__setting_button.get_rect(topleft=(900, 300))
 
+    def __load_buttons(self):
+        self.__button = {
+            "back": self.__back_button_rect,
+            "level": self.__play_button_rect,
+            "setting": self.__setting_button_rect,
+        }
+
     def get_surface(self):
         return self.__surface
 
+    def get_button(self, name):
+        return self.__button[name]
+
     def draw_home(self):
-        self.__surface.blit(self.__background, (0, 0))
+        self.__surface.blit(self.__home_background, (0, 0))
         self.__surface.blit(self.__play_button, self.__play_button_rect)
         self.__surface.blit(self.__setting_button, self.__setting_button_rect)
 
     def draw_level(self):
-            self.__surface.blit(self.__background, (0, 0))
+            self.__surface.blit(self.__level_background, (0, 0))
             self.__surface.blit(self.__back_button, self.__back_button_rect)
 
     def draw_setting(self):
-            self.__surface.blit(self.__background, (0, 0))
+            self.__surface.blit(self.__setting_background, (0, 0))
             self.__surface.blit(self.__back_button, self.__back_button_rect)
 
     def render(self):
